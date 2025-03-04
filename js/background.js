3adbcdd76f8525cf8b010e0590fd359e6167b27a
smini01 <168179572+smini01@users.noreply.github.com>
2025-03-04 13:32:08 +0900

    Update background.js

--- js/background.js
+++ js/background.js
@@ -1,9 +1,8 @@
-const images = ["0.jpeg", "1.jpeg", "2.jpeg", "3.jpeg"];
+const images = ["3.jpeg"];
 
-const chosenImage = images[Math.floor(Math.random() * images.length)];
+const chosenImage = images[0];
 
 const bgImage = document.createElement("img");
-
 bgImage.src = `img/${chosenImage}`;
 
 document.body.appendChild(bgImage);
